fa098d99bde3a471df5ef0fed8833ca91a1e21cf
Naeem Prasla <86165509+naeemprasla@users.noreply.github.com>
2021-10-17 01:32:34 +0500

    Update DisbaleBlockEditorAndWidgets.php

--- DisbaleBlockEditorAndWidgets.php
+++ DisbaleBlockEditorAndWidgets.php
@@ -2,6 +2,7 @@
 
 // Disables the block editor from managing widgets in the Gutenberg plugin.
 add_filter( 'gutenberg_use_widgets_block_editor', '__return_false', 100 );
+add_filter('use_block_editor_for_post', '__return_false', 10);
 
 // Disables the block editor from managing widgets. renamed from wp_use_widgets_block_editor
 add_filter( 'use_widgets_block_editor', '__return_false' );
