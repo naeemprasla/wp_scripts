290a9fa59c994cdec7844a1dde559a53ab5694db
Naeem Prasla <86165509+naeemprasla@users.noreply.github.com>
2022-01-24 20:27:29 +0500

    Update php-mail-headers.php

--- php-mail-headers.php
+++ php-mail-headers.php
@@ -4,8 +4,8 @@ $headers = "MIME-Version: 1.0" . "\r\n";
 $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 // More headers
-$headers .= 'From: Fincoi <no-reply@fincoi.com>' . "\r\n";
-$headers .= 'Cc: no-reply@fincoi.com' . "\r\n";
-$headers .= "Reply-To: no-reply@fincoi.com" . "\r\n";		   
-$headers .= "BCC: no-reply@fincoi.com" . "\r\n";		   
-$headers .= "Return-Path: no-reply@fincoi.com" . "\r\n";	
+$headers .= 'From: Site <no-reply@domain.com>' . "\r\n";
+$headers .= 'Cc: no-reply@domain.com' . "\r\n";
+$headers .= "Reply-To: no-reply@domain.com" . "\r\n";		   
+$headers .= "BCC: no-reply@domain.com" . "\r\n";		   
+$headers .= "Return-Path: no-reply@domain.com" . "\r\n";	
