a9f43b6f37c7b5b6ff03a0f0d47b29228a7c6d76
Cryptonion Codes <86165509+cryptonioncodes@users.noreply.github.com>
2022-11-15 00:22:55 +0500

    Update wp_dynamic_widgets.php

--- wp_dynamic_widgets.php
+++ wp_dynamic_widgets.php
@@ -1,21 +1,17 @@
 <?php 
 
   $sidebars = array(
-            array( 'name' => __( 'General Sidebar', 'dokan-theme' ), 'id' => 'sidebar-1' ),
-            array( 'name' => __( 'Home Sidebar', 'dokan-theme' ), 'id' => 'sidebar-home' ),
-            array( 'name' => __( 'Blog Sidebar', 'dokan-theme' ), 'id' => 'sidebar-blog' ),
-            array( 'name' => __( 'Header Sidebar', 'dokan-theme' ), 'id' => 'sidebar-header' ),
-            array( 'name' => __( 'Shop Archive', 'dokan-theme' ), 'id' => 'sidebar-shop' ),
-            array( 'name' => __( 'Single Product', 'dokan-theme' ), 'id' => 'sidebar-single-product' ),
-            array( 'name' => __( 'Footer Sidebar - 1', 'dokan-theme' ), 'id' => 'footer-1' ),
-            array( 'name' => __( 'Footer Sidebar - 2', 'dokan-theme' ), 'id' => 'footer-2' ),
-            array( 'name' => __( 'Footer Sidebar - 3', 'dokan-theme' ), 'id' => 'footer-3' ),
-            array( 'name' => __( 'Footer Sidebar - 4', 'dokan-theme' ), 'id' => 'footer-4' ),
-            array( 'name' => __( 'Footer Sidebar - 5 (App Images)', 'dokan-theme' ), 'id' => 'footer-5' ),
-            array( 'name' => __( 'Footer Sidebar - 6 (Social media)', 'dokan-theme' ), 'id' => 'footer-6' ),
+            array( 'name' => __( 'General Sidebar', 'theme' ), 'id' => 'sidebar-1' ),
+            array( 'name' => __( 'Home Sidebar', 'theme' ), 'id' => 'sidebar-home' ),
+            array( 'name' => __( 'Blog Sidebar', 'theme' ), 'id' => 'sidebar-blog' ),
+            array( 'name' => __( 'Header Sidebar', 'theme' ), 'id' => 'sidebar-header' ),
+            array( 'name' => __( 'Footer Sidebar - 1', 'theme' ), 'id' => 'footer-1' ),
+            array( 'name' => __( 'Footer Sidebar - 2', 'theme' ), 'id' => 'footer-2' ),
+            array( 'name' => __( 'Footer Sidebar - 3', 'theme' ), 'id' => 'footer-3' ),
+            array( 'name' => __( 'Footer Sidebar - 4', 'theme' ), 'id' => 'footer-4' ),
         );
 
-        $args = apply_filters( 'dokan_widget_args', array(
+        $args = apply_filters( 'theme_widget_args', array(
             'before_widget' => '<aside id="%1$s" class="widget %2$s">',
             'after_widget'  => '</aside>',
             'before_title'  => '<h3 class="widget-title">',
